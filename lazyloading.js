5596d628382dcc2d2977b2306a188ba3e2c80dad
madiq7 <magdalenapiotrowska07@gmail.com>
2020-05-05 15:00:18 +0200

    próba

---
+++ lazyloading.js
@@ -0,0 +1,24 @@
+import Bound from './bounds.js'
+
+const boundary = Bound({
+    margins: {bottom: -100}
+})
+
+const image = document.querySelectorAll('img')
+console.log('show!')
+
+const whenImageEnters = (image) => {
+    return () => {
+    image.src = image.dataset.src
+    //image.classList.add('reveal');
+    boundary.unWatch(image)
+    console.log('show!')
+    }
+}
+
+image.forEach(img => {
+    boundary.watch(img,whenImageEnters(img))
+})
+
+
+
